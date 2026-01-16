a68d9d3eb8def49ad158c9fccd09bb927f2f6a7a
AbhishekMule13 <abhimule1308@gmail.com>
2026-01-16 13:10:19 +0530

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,17 @@
+public class DataType {
+    public static void main(String[] args) {
+        int a=13;
+		float b = 12.23f;
+		char c = 'A';
+		boolean d = true;
+		String e = "Abhishek";
+		
+		
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+    }
+    
+}
